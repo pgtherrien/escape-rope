06ab13f4d35d7e4cc3c7ee969933fdd6ba1d6552
Peter <peter@Peters-MacBook-Pro.local>
2019-06-03 23:00:06 -0400

    Accessing pokedex

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,14 +1,46 @@
 import React from "react";
+import { Spinner } from "@blueprintjs/core";
 
+import firebase from "../../firebase";
 import Header from "../Header";
 import ListNavbar from "./ListNavbar";
 
 class List extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      pokedex: []
+    };
+  }
+
+  componentWillMount() {
+    const pokemonRef = firebase.database().ref("pokemon");
+    pokemonRef.on("value", snapshot => {
+      let pokemon = snapshot.val();
+      this.setState({ pokedex: pokemon });
+    });
+  }
+
+  renderDex = () => {
+    const { pokedex } = this.state;
+
+    if (pokedex.length > 0) {
+      let renderedDex = [];
+      Object.keys(pokedex).forEach(function(number) {
+        renderedDex.push(<li key={number}>{pokedex[number].Name}</li>);
+      });
+      return <span>hello world</span>;
+    } else {
+      return <Spinner />;
+    }
+  };
+
   render() {
     return (
       <div>
         <Header path={this.props.location.pathname} />
         <ListNavbar />
+        {this.renderDex()}
       </div>
     );
   }
